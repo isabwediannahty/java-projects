da2fad573652b44ecd8c5f3580e8997fd28a4700
isabwediannahty <149628036+isabwediannahty@users.noreply.github.com>
2023-11-03 11:38:03 +0200

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,22 @@
+ class ifelse{
+    public static void main(String args[]){  
+    int salary=100000;  
+     
+    if (salary>90000)
+    {
+    System.out.println("very rich");
+    }
+    if(salary>80000)
+    {
+        System.out.println("rich");
+    } 
+    if(salary>60000)
+    {
+        System.out.println("middle");
+    
+    }
+       else
+      {
+           System.out.println("poor");
+      }
+    }}  
